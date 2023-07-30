c16a06680d4caf1e82066f94445ab59c071a2a6a
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-07-30 11:48:51 +0700

    KN5 loader stub

---
+++ kn_loader.gd
@@ -0,0 +1,9 @@
+class_name KNLoader
+extends RefCounted
+## Loader class for the KN5 format.
+##
+## Because of how little documentation there is on the KN5 format, this importer
+## is mostly based on [url=https://github.com/RaduMC/kn5-converter]an existing
+## program that converts KN5 to FBX and OBJ[/url]. The original program's output
+## is a little screwed up (more specifically the normals), but the codes are
+## very readable and easy to understand.
