fca9e5b2eb00231e537f0df9834d5f41c0c5255a
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-07-30 15:37:30 +0700

    Implement loading nodes

--- kn_loader.gd
+++ kn_loader.gd
@@ -12,6 +12,7 @@ extends RefCounted
 var version: int
 var textures: Array[KNTexture]
 var materials: Array[KNMaterial]
+var root_node: KNNode
 
 var _file: FileAccess
 
@@ -61,13 +62,152 @@ func load(path: String) -> Error:
             mat.textures[sample_name] = tex_name
         
         materials.append(mat)
+    
+    root_node = _load_node(true)
 
     return OK
+
+
+func _load_node(root: bool, parent: KNNode = null) -> KNNode:
+    var node := KNNode.new()
+    node.type = _file.get_32()
+    node.name = _get_p_string()
+    var child_count := _file.get_32() # This is somehow not right before child nodes
+    _file.get_8() # Unknown yet again
+
+    match node.type:
+        1: # Dummy node
+            for row in 4:
+                for col in 4:
+                    node.transform_matrix[row][col] = _file.get_float()
+            
+            node.translation = Vector3(node.transform_matrix[3][0], node.transform_matrix[3][1], node.transform_matrix[3][2])
+            node.rotation = _to_euler(node.transform_matrix)
+            node.scale = _to_scale(node.transform_matrix)
+        2: # Mesh
+            for i in 3:
+                _file.get_8() # Unknown
+
+            for i in _file.get_32(): # Vertices
+                var position: Vector3
+                var normal: Vector3
+                var uv: Vector2
+
+                position.x = _file.get_float()
+                position.y = _file.get_float()
+                position.z = _file.get_float()
+
+                normal.x = _file.get_float()
+                normal.y = _file.get_float()
+                normal.z = _file.get_float()
+
+                uv.x = _file.get_float()
+                uv.y = _file.get_float()
+
+                node.positions.append(position)
+                node.normals.append(normal)
+                node.uvs.append(uv)
+
+                _file.seek(_file.get_position() + 12) # Tangents
+            
+            for i in _file.get_32(): # Indices
+                node.indices.append(_file.get_16())
+            
+            node.material_id = _file.get_32()
+            _file.seek(_file.get_position() + 29) # Unknown
+        3: # Animated mesh
+            for i in 3:
+                _file.get_8() # Unknown
+            
+            for i in _file.get_32(): # Bone names
+                var bone_name := _get_p_string()
+                _file.seek(_file.get_position() + 64) # Transform matrix
+            
+            for i in _file.get_32(): # Vertices
+                var position: Vector3
+                var normal: Vector3
+                var uv: Vector2
+
+                position.x = _file.get_float()
+                position.y = _file.get_float()
+                position.z = _file.get_float()
+
+                normal.x = _file.get_float()
+                normal.y = _file.get_float()
+                normal.z = _file.get_float()
+
+                uv.x = _file.get_float()
+                uv.y = _file.get_float()
+
+                node.positions.append(position)
+                node.normals.append(normal)
+                node.uvs.append(uv)
+
+                _file.seek(_file.get_position() + 44) # Tangents and weights
+            
+            for i in _file.get_32(): # Indices
+                node.indices.append(_file.get_16())
+            
+            node.material_id = _file.get_32()
+            _file.seek(_file.get_position() + 12) # Unknown
+
+    if root:
+        node.hierarchy_matrix = node.transform_matrix
+    else:
+        node.hierarchy_matrix = _matrix_mult(node.transform_matrix, parent.hierarchy_matrix)
+    
+    for i in child_count:
+        node.children.append(_load_node(false, node))
+
+    return node
 
 
 func _get_p_string() -> String:
     var len := _file.get_32()
     return _file.get_buffer(len).get_string_from_ascii()
+
+
+func _to_euler(transform: Array) -> Vector3:
+    var heading: float
+    var attitude: float
+    var bank: float
+
+    if transform[0][1] > 0.998:
+        heading = atan2(-transform[1][0], transform[1][1])
+        attitude = -PI / 2
+        bank = 0
+    elif transform[0][1] < -0.998:
+        heading = atan2(-transform[1][0], transform[1][1])
+        attitude = PI / 2
+        bank = 0
+    else:
+        heading = atan2(transform[0][1], transform[0][0])
+        bank = atan2(transform[1][2], transform[2][2])
+        attitude = asin(-transform[0][2])
+
+    attitude *= 180 / PI
+    heading *= 180 / PI
+    bank *= 180 / PI
+    return Vector3(bank, attitude, heading)
+
+
+func _to_scale(transform: Array) -> Vector3:
+    var x := sqrt(transform[0][0] * transform[0][0] + transform[1][0] * transform[1][0] + transform[2][0] * transform[2][0])
+    var y := sqrt(transform[0][1] * transform[0][1] + transform[1][1] * transform[1][1] + transform[2][1] * transform[2][1])
+    var z := sqrt(transform[0][2] * transform[0][2] + transform[1][2] * transform[1][2] + transform[2][2] * transform[2][2])
+    return Vector3(x, y, z)
+
+
+func _matrix_mult(mat_a: Array, mat_b: Array) -> Array:
+    var result := []
+    for row in 4:
+        result.append([0.0, 0.0, 0.0, 0.0])
+    
+    for i in 4:
+        for j in 4:
+            result[i][j] = mat_a[i][0] * mat_b[0][j] + mat_a[i][1] * mat_b[1][j] + mat_a[i][2] * mat_b[2][j] + mat_a[i][3] * mat_b[3][j]
+    
+    return result
 
 
 class KNTexture extends RefCounted:
@@ -82,3 +222,31 @@ class KNMaterial extends RefCounted:
     var shader: String
     var properties: Dictionary
     var textures: Dictionary
+
+
+class KNNode extends RefCounted:
+    var type: int
+    var name: String
+    var children: Array[KNNode]
+    
+    var transform_matrix: Array
+    var hierarchy_matrix: Array
+    var translation: Vector3
+    var rotation: Vector3
+    var scale: Vector3
+
+    # For mesh nodes
+    var positions: Array[Vector3]
+    var normals: Array[Vector3]
+    var uvs: Array[Vector2]
+    var indices: Array[int]
+    var material_id: int
+
+    # For animated mesh nodes
+    var bone_count: int
+
+
+    func _init() -> void:
+        for row in 4:
+            transform_matrix.append([0.0, 0.0, 0.0, 0.0])
+            hierarchy_matrix.append([0.0, 0.0, 0.0, 0.0])
